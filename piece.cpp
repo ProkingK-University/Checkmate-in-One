d3c5e73d667da66ea963bee6b73f78b6998a7294
Ashley Kapaso <ashleykapaso@gmail.com>
2022-07-19 17:20:51 +0200

    Impleamentation shell

--- piece.cpp
+++ piece.cpp
@@ -1,1 +1,43 @@
 #include "piece.h"
+
+piece::piece() {}
+
+piece::piece(piece* newPiece)
+{
+
+}
+
+piece::piece(std::string pType, char side, int x, int y)
+{
+
+}
+
+void piece::setX(int x) {}
+void piece::setY(int y) {}
+
+int piece::getX() {}
+int piece::getY() {}
+
+char piece::getSide() {}
+
+std::string piece::getPieceType() {}
+
+void piece::operator[](int pos)
+{
+
+}
+
+piece& piece::operator+(std::string move)
+{
+
+}
+
+std::ostream& operator<<(std::ostream& output, const piece& t)
+{
+
+}
+
+piece::~piece()
+{
+
+}
