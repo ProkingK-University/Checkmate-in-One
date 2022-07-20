5162d36d37efc3f86af578eb6900d8a7656fac9f
Ashley Kapaso <ashleykapaso@gmail.com>
2022-07-20 14:15:59 +0200

    Impleamentation of piece class

--- piece.cpp
+++ piece.cpp
@@ -4,40 +4,64 @@ piece::piece() {}
 
 piece::piece(piece* newPiece)
 {
-
+    this->xPos = newPiece->getX();
+    this->yPos = newPiece->getY();
+    this->side = newPiece->getSide();
+    this->pieceType = newPiece->getPieceType();
 }
 
-piece::piece(std::string pType, char side, int x, int y)
-{
+piece::piece(std::string pType, char side, int x, int y) : pieceType(pType), side(side), xPos(x), yPos(y) {}
 
-}
+void piece::setX(int x) {xPos = x;}
+void piece::setY(int y) {yPos = y;}
 
-void piece::setX(int x) {}
-void piece::setY(int y) {}
+int piece::getX() {return xPos;}
+int piece::getY() {return yPos;}
 
-int piece::getX() {}
-int piece::getY() {}
+char piece::getSide() {return side;}
 
-char piece::getSide() {}
-
-std::string piece::getPieceType() {}
+std::string piece::getPieceType() {return pieceType;}
 
 void piece::operator[](int pos)
 {
-
+    if (pos == 0 || pos == 1)
+    {
+        if (pos == 0)
+        {
+            std::cout<< "x coord: " << xPos <<std::endl;
+        }
+        else
+        {
+            std::cout<< "y coord: " << yPos <<std::endl;
+        }
+    }
+    else
+    {
+        std::cout<< "Invalid Index" <<std::endl;
+    }
 }
 
 piece& piece::operator+(std::string move)
 {
+    if (((int)move[0]-48 >= 0 && (int)move[0]-48 <= 7) && ((int)move[2]-48 >= 0 && (int)move[2]-48 <= 7))
+    {
+        xPos = (int)move[0]-48;
+        yPos = (int)move[2]-48;
+    }
 
+    return *this;
 }
 
 std::ostream& operator<<(std::ostream& output, const piece& t)
 {
+    output<< t.side << " " << t.pieceType << " at [" << t.xPos << "," << t.yPos << "]" <<std::endl;
 
+    return output;
 }
 
 piece::~piece()
 {
-
+    std::cout<< "(" << xPos << "," << yPos << ")" << " " << side << " " << pieceType << " deleted" <<std::endl;
 }
+
+// ;)
