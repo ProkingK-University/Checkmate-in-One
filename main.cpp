8b51cfbcaf8cdbf660061fd9f081bff013c2aec6
Ashley Kapaso <ashleykapaso@gmail.com>
2022-09-10 09:33:11 +0200

    Added testing files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
-#include "piece.h"
+#include "solver.h"
 
 int main()
 {
+    piece p("bishop", 'b', 2, 3);
+
+    std::cout<<p;
+
+    p[1];
+
+    p = p + "2,3";
+
+    std::cout<<p;
+
+
+    board b("game1.txt");
+    b.print();
+
+    std::cout<<std::endl;
+
+    --b;
+
+    solver s("games.txt");
+
     return 0;
 }
